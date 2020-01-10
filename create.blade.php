e9ef6bf162d00412118ec461361be1c29cdb6905
yamato0703 <omata.yamato.07034649@docomo.me.jp>
2020-01-10 10:07:21 +0000

    Lalavel10

---
+++ create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="uft-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="widh=dvice-width, initial-scale=1">
+      
+      <title >MyPlofile</title>
+    </head>
+    <body>
+      <h1>add Action</h1>  
+    </body>
+</html>
